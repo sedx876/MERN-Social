b1024c635e9e093432d41d027cccb526bd49dc2c
sedx876 <swatkins876@gmail.com>
2021-06-02 13:21:25 -0400

    Added Home.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import './App.css'
 import Navbar from './components/Navbar'
+import Home from './components/screens/Home'
 import { BrowserRouter as Router, Route} from 'react-router-dom'
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
     <div className="App">
       <Router>
         <Navbar/>
-        <Route path='/'></Route>
+        <Route path='/' component={Home}></Route>
       </Router>
     </div>
   );
