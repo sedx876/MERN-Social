9d303d7e0769af26b9472d3873e1b72e152c70cb
sedx876 <swatkins876@gmail.com>
2021-06-02 13:17:26 -0400

    Added Navbar.js

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+			<nav>
+    		<div className="nav-wrapper gradient">
+      		<a href="#" className="brand-logo left">QueenTeens</a>
+      		<ul id="nav-mobile" className="right">
+        		<li><a href="sass.html">Sass</a></li>
+        		<li><a href="badges.html">Components</a></li>
+        		<li><a href="collapsible.html">JavaScript</a></li>
+      		</ul>
+    		</div>
+  		</nav>
+			
+    )
+}
+
+export default Navbar
