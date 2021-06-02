009c2fbb740073bb1efcbaf80caa80b7c9c64dc3
sedx876 <swatkins876@gmail.com>
2021-06-02 11:43:22 -0400

    update

--- server/app.js
+++ server/app.js
@@ -1,32 +1,30 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const {MONGOURI} = require('./valuekeys.js')
+const express = require("express");
 const app = express()
-
+const mongoose = require("mongoose");
 const PORT = 5000
+const {MONGOURI} = require("./valuekeys.js")
 
-require('./models/user')
-require('./models/post')
+//dDXxLk5YEuyH2UFB - database access password
+
+
+mongoose.connect(MONGOURI,{
+    useNewUrlParser: true ,
+    useUnifiedTopology: true
+});
+mongoose.connection.on('connected',()=>{
+    console.log("We are connected to the server i.e Mongo DB");
+})
+mongoose.connection.on('error',()=>{
+    console.log("We are not connected to the server i.e Mongo DB");
+})
+
+require("./models/user")
+require("./models/post")
 app.use(express.json())
-
-mongoose.connect(MONGOURI, 
-    {useNewUrlParser: true, 
-     useUnifiedTopology: true}
-)
-
-mongoose.connection.on('connected', () => {
-    console.log('We are connected')
-})
-
-mongoose.connection.on('error', () => {
-    console.log('We are NOT connected')
-})
-
 app.use(require('./routes/authen.js'))
+app.use(require('./routes/post.js'))
 
-
-
-app.listen(PORT, () => {
-    console.log('Server is running at', PORT)
+app.listen(PORT,()=>{
+    console.log("Server is running at ",PORT)
 })
 
--- server/valuekeys.js
+++ server/valuekeys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    MONGOURI: "mongodb+srv://admin:Judyth28@socialnetwork.nhbht.mongodb.net/socialnetwork?retryWrites=true&w=majority"
+    MONGOURI: "mongodb+srv://admin:Judyth28@socialnetwork.nhbht.mongodb.net/socialnetwork?retryWrites=true&w=majority",
+    JWT_SECRET:"no743q6rjuewtleurjtnyut"
 }
