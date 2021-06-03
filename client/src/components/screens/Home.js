974397395682794957ee49b6d9fe70fe973e47d9
sedx876 <swatkins876@gmail.com>
2021-06-03 11:42:05 -0400

    work on Home.js

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -2,8 +2,78 @@ import React from 'react'
 
 const Home = () => {
 	return (
-		<div>
-			<h1>Homo Page</h1>
+		<div className='home'>
+			<div className='card home-card center pink lighten-1'>
+				<h5>AndyCandy</h5>
+				<div className='card-image pink lighten-1'>
+				<img 
+					className='pink lighten-1'
+					src={'https://cdn.pixabay.com/photo/2020/02/13/06/49/seascape-4844697__340.jpg'}
+				/>
+				</div>
+				<div className='card-content'>
+					<i className='material-icons' 
+						style={{color:'pink'}}>thumb_up</i>
+					<h6>Title of Da Card</h6>
+					<br/>
+					<p>Awesome Sauce Normally, both your asses would be dead as 
+						fucking fried chicken, but you happen to pull this shit while 
+						I'm in a transitional period so I don't wanna kill you, I wanna 
+						help you. But I can't give you this case, it don't belong to me. 
+						Besides, I've already been through too much shit this morning over 
+						this case to hand it over to your dumb ass.
+					</p>
+					<input type='text' placeholder='Add a Comment'/>
+				</div>
+			</div>
+
+			<div className='card home-card center pink lighten-1'>
+				<h5>AndyCandy</h5>
+				<div className='card-image pink lighten-1'>
+				<img 
+					className='pink lighten-1'
+					src={'https://cdn.pixabay.com/photo/2020/12/14/11/18/cat-5830643__340.jpg'}
+				/>
+				</div>
+				<div className='card-content'>
+					<i className='material-icons' 
+						style={{color:'pink'}}>thumb_up</i>
+					<h6>Title of Da Card</h6>
+					<br/>
+					<p>Awesome Sauce Normally, both your asses would be dead as 
+						fucking fried chicken, but you happen to pull this shit while 
+						I'm in a transitional period so I don't wanna kill you, I wanna 
+						help you. But I can't give you this case, it don't belong to me. 
+						Besides, I've already been through too much shit this morning over 
+						this case to hand it over to your dumb ass.
+					</p>
+					<input type='text' placeholder='Add a Comment'/>
+				</div>
+			</div>
+
+			<div className='card home-card center pink lighten-1'>
+				<h5>AndyCandy</h5>
+				<div className='card-image pink lighten-1'>
+				<img 
+					className='pink lighten-1'
+					src={'https://cdn.pixabay.com/photo/2020/09/26/08/05/amusement-park-5603424__340.jpg'}
+				/>
+				</div>
+				<div className='card-content'>
+					<i className='material-icons' 
+						style={{color:'pink'}}>thumb_up</i>
+					<h6>Title of Da Card</h6>
+					<br/>
+					<p>Awesome Sauce Normally, both your asses would be dead as 
+						fucking fried chicken, but you happen to pull this shit while 
+						I'm in a transitional period so I don't wanna kill you, I wanna 
+						help you. But I can't give you this case, it don't belong to me. 
+						Besides, I've already been through too much shit this morning over 
+						this case to hand it over to your dumb ass.
+					</p>
+					<input type='text' placeholder='Add a Comment'/>
+				</div>
+			</div>
 		</div>
 	)
 }
